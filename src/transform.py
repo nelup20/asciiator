97faa5cdf9fefef6f6a7c08c0557ea416690231d
nelup20 <nelu.platonov@gmail.com>
2022-06-01 22:54:51 +0200

    Small refactor

--- src/transform.py
+++ src/transform.py
@@ -5,11 +5,10 @@ from random import randint
 def transform_jpg(file):
     new_circle_color = (randint(0, 255), randint(0, 255), randint(0, 255))
 
-    image_pixels = file.get_data()
-    new_image_pixels = list(
-        map(lambda pixel: (0, 0, 0) if pixel == (255, 255, 255) else new_circle_color, image_pixels))
+    image_data = file.get_data()
+    new_image_data = list(map(lambda pixel: (0, 0, 0) if pixel == (255, 255, 255) else new_circle_color, image_data))
 
-    file.create_new_file(new_image_pixels)
+    file.create_new_file(new_image_data)
 
 
 def transform_mp4(file):
