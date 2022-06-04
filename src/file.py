527548853a6ca2803ef3c7fb4197a2042810bace
nelup20 <nelu.platonov@gmail.com>
2022-06-04 23:04:51 +0200

    Add basic/working JPEG to ASCII transformation 🥳🎉

--- src/file.py
+++ src/file.py
@@ -23,11 +23,16 @@ class File:
         self.type = FileType.get_file_type(self.get_name_with_extension())
 
         if self.type is FileType.Image:
-            self.opened_file = Image.open(self.absolute_path)
+            self.opened_file = Image.open(self.absolute_path).convert("L")
 
     @classmethod
     def is_input_file(cls, arg: str) -> bool:
         return arg.endswith((".jpg", ".jpeg", ".mp4"))
+
+    @classmethod
+    def create_new_file(cls, data, path) -> None:
+        with open(abspath(path), "w") as new_file:
+            new_file.write(data)
 
     def get_data(self) -> bytearray:
         return self.opened_file.getdata()
@@ -35,10 +40,11 @@ class File:
     def get_name_with_extension(self) -> str:
         return f"{self.name}.{self.extension}"
 
-    def create_new_file(self, transformed_data) -> None:
-        new_file = Image.new(self.opened_file.mode, self.opened_file.size)
-        new_file.putdata(transformed_data)
-        new_file.save(abspath(f"./{self.name}_asciiator.{self.extension}"))
+    def get_width(self) -> int:
+        return self.opened_file.width
+
+    def get_height(self) -> int:
+        return self.opened_file.height
 
     def __str__(self) -> str:
         return f'File @ {hex(id(self))}: name = {self.name}, path = {self.relative_path}, type = {self.type}'
