6f59e71a2ac9a5c2ab9b7a34275fec82366fb8c3
Nelu Platonov <nelu.platonov@gmail.com>
2023-03-31 23:43:37 +0200

    Debug actions

--- test/file/test_video_file.py
+++
@@ -1,8 +0,0 @@
-from src.file.video_file import VideoFile
-
-
-class TestVideoFileInstance:
-    file = VideoFile("./test/resource/video/test.mp4")
-
-    def test_video_file_get_data(self):
-        pass
