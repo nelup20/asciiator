fa17cec31f3a80e47882a04a2bd8837da9219b88
Nelu Platonov <nelu.platonov@gmail.com>
2023-03-31 23:58:56 +0200

    Debug test

--- src/file/video_file.py
+++ src/file/video_file.py
@@ -33,6 +33,7 @@ class VideoFile(File):
             get_framerate_subprocess = subprocess.run(
                 f"ffprobe -v 0 -of compact=p=0 -select_streams 0 -show_entries stream=r_frame_rate {self.absolute_path}",
                 capture_output=True,
+                shell=True,
             )
             framerate = (
                 str(get_framerate_subprocess.stdout, "utf-8").strip().split("=")[1]
