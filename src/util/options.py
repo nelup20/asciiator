b387c83befdbe73cd6f8249ca7d76a6f96de53a2
Nelu <nelu.platonov@gmail.com>
2023-05-23 23:41:09 +0200

    Use argparse for options + other improvements (#28)
    * Use argparse for args, rename pipeline, switch default values for colors
    
    * Fix bug where all the pixels of the first row are skipped after the first pixel
    
    * Add Jetbrains dir to .gitignore
    
    * Update README & fix test

--- src/util/options.py
+++ src/util/options.py
@@ -1,95 +1,96 @@
-import sys
+from argparse import ArgumentParser
 from os import path
 from typing import List
-from warnings import warn
 
 from src.file.file import File, FileType
 from src.file.gif_file import GifFile
 from src.file.image_file import ImageFile
 from src.file.video_file import VideoFile
 
-HELP_MESSAGE = """
-Asciiator has the following optional flags:
--inplace                Modify the file inplace. 
-                        The original file will be transformed to ASCII and no new output file will be generated.
-
--help                   Display the help documentation
-
--inverted               Invert the colors for the background and text. 
-                        By default, the background color is white (255) and the text color is black (0). 
-                        If -inverted is present, the background will be black and the text color will be white.
-
--text_file              Save the transformed ASCII art to a .txt file as well 
-                        (only applicable when the input is a image)
-
---reduction=x           Reduce the output by a factor of x (int). Default value: 4. 
-                        Example: --reduction=2 to convert half of the pixels to ASCII
-
---output_path="x"       Specify the output path where all new files will be created. 
-                        By default it's the current directory. 
-                        Warning: if the -inplace flag is present, then this flag will be ignored, 
-                        so the original absolute path will be used.
-"""
+VERSION = "Asciiator 2.0.0"
 
 
 class Options:
     def __init__(self) -> None:
-        self.inplace = False
-        self.text_file = False
         self.input: List[File] = []
-        self.reduction_factor = 4
-        self.inverted_colors = False
-        self.output_path = "."
 
-        self._HELP_FLAG_MESSAGE = "Use -help to see all available flags."
+        arg_parser = ArgumentParser(
+            prog="asciiator",
+            description="Transform any Image or Video file into ASCII",
+            allow_abbrev=False,
+        )
 
-        if len(sys.argv) > 1:
-            for arg in enumerate(sys.argv):
-                if arg[0] == 0:
-                    continue
+        arg_parser.add_argument(
+            "input_files",
+            nargs="+",
+            help=f"The paths to each input file. Supported file types: {FileType.get_supported_file_extensions()}",
+        )
 
-                if File.is_input_file(arg[1]):
-                    file_path = arg[1]
-                    file_type = FileType.get_file_type(file_path)
+        arg_parser.add_argument(
+            "--inplace",
+            action="store_true",
+            help="Modify the file inplace. The original file will be transformed to ASCII and no new output file will "
+            "be generated.",
+        )
 
-                    if not path.exists(file_path):
-                        raise FileNotFoundError(file_path)
+        arg_parser.add_argument(
+            "--inverted",
+            action="store_true",
+            help="Invert the colors for the background and text. By default, the background color is black (0) and "
+            "the text color is white (255). If -inverted is present, the background will be white and the text "
+            "color will be black.",
+        )
 
-                    if file_type is FileType.Image:
-                        self.input.append(ImageFile(file_path))
+        arg_parser.add_argument(
+            "--text_file",
+            action="store_true",
+            help="Save the transformed ASCII art to a .txt file as well (only applicable when the input is a image)",
+        )
 
-                    if file_type is FileType.Video:
-                        self.input.append(VideoFile(file_path))
+        arg_parser.add_argument(
+            "--reduction",
+            type=int,
+            default=4,
+            help="Reduce the output by a factor of x (int). Default value: 4. Example: --reduction 2 to convert half "
+            "of the pixels to ASCII",
+        )
 
-                    if file_type is FileType.Gif:
-                        self.input.append(GifFile(file_path))
+        arg_parser.add_argument(
+            "--output_path",
+            type=str,
+            default=".",
+            help="Specify the output path where all new files will be created. By default it's the current directory. "
+            "Warning: if the -inplace flag is present, then this flag will be ignored, so the original absolute "
+            "path will be used.",
+        )
 
-                    continue
+        arg_parser.add_argument("--version", action="version", version=VERSION)
 
-                if "--reduction=" in arg[1]:
-                    self.reduction_factor = int(arg[1].split("=")[1])
-                    continue
+        args = arg_parser.parse_args()
 
-                if "--output_path=" in arg[1]:
-                    self.output_path = arg[1].split("=")[1]
-                    warn(
-                        "Warning: if the -inplace flag is present, the original absolute path will be used and "
-                        "the custom output_path will be ignored."
-                    )
-                    continue
+        self.inplace = args.inplace
+        self.inverted_colors = args.inverted
+        self.text_file = args.text_file
+        self.reduction_factor = args.reduction
+        self.output_path = args.output_path
 
-                match arg[1]:
-                    case "-inplace":
-                        self.inplace = True
-                    case "-text_file":
-                        self.text_file = True
-                    case "-help":
-                        sys.exit(HELP_MESSAGE)
-                    case "-inverted":
-                        self.inverted_colors = True
-                    case _:
-                        raise Exception(
-                            f"Argument #{arg[0]} is invalid: {arg[1]}. {self._HELP_FLAG_MESSAGE}"
-                        )
-        else:
-            raise Exception(f"No arguments provided. {self._HELP_FLAG_MESSAGE}")
+        for file_path in args.input_files:
+            if File.is_input_file(file_path):
+                file_type = FileType.get_file_type(file_path)
+
+                if not path.exists(file_path):
+                    raise FileNotFoundError(file_path)
+
+                if file_type is FileType.Image:
+                    self.input.append(ImageFile(file_path))
+
+                if file_type is FileType.Video:
+                    self.input.append(VideoFile(file_path))
+
+                if file_type is FileType.Gif:
+                    self.input.append(GifFile(file_path))
+            else:
+                raise Exception(
+                    f"Invalid file type for input: {file_path}\n"
+                    f"Supported file types: {FileType.get_supported_file_extensions()}"
+                )
--- test/util/test_options.py
+++ test/util/test_options.py
@@ -1,78 +1,80 @@
 import pytest
 
 from src.file.file import FileType
-from src.util.options import Options, HELP_MESSAGE
+from src.util.options import Options
 
 from test.helpers import clean_up_sys_argv, handle_sys_args
 
 clean_up_sys_argv()
 
+TEST_IMAGE_PATH = "./test/resource/img/input/test.jpg"
+TEST_VIDEO_PATH = "./test/resource/video/input/test.mp4"
+
 
 class TestOptions:
-    def test_options_no_arguments(self):
-        try:
-            Options()
-        except Exception as exception:
-            assert (
-                f"{exception}"
-                == "No arguments provided. Use -help to see all available flags."
-            )
-
     @pytest.mark.parametrize("handle_sys_args", [["invalid_arg_123"]], indirect=True)
     def test_options_invalid_argument(self, handle_sys_args):
         try:
             Options()
         except Exception as exception:
             assert (
-                f"{exception}"
-                == "Argument #1 is invalid: invalid_arg_123. Use -help to see all available flags."
+                f"{exception}" == f"Invalid file type for input: invalid_arg_123\n"
+                f"Supported file types: {FileType.get_supported_file_extensions()}"
             )
 
-    @pytest.mark.parametrize("handle_sys_args", [["-inplace"]], indirect=True)
+    @pytest.mark.parametrize(
+        "handle_sys_args", [[TEST_IMAGE_PATH, "--inplace"]], indirect=True
+    )
     def test_options_inplace(self, handle_sys_args):
         options = Options()
 
         assert options.inplace
 
-    @pytest.mark.parametrize("handle_sys_args", [["--reduction=3"]], indirect=True)
+    @pytest.mark.parametrize(
+        "handle_sys_args", [[TEST_IMAGE_PATH, "--reduction", "3"]], indirect=True
+    )
     def test_options_reduction(self, handle_sys_args):
         options = Options()
 
         assert options.reduction_factor == 3
 
-    @pytest.mark.parametrize("handle_sys_args", [["-text_file"]], indirect=True)
+    @pytest.mark.parametrize(
+        "handle_sys_args", [[TEST_IMAGE_PATH, "--text_file"]], indirect=True
+    )
     def test_options_text_file(self, handle_sys_args):
         options = Options()
 
         assert options.text_file
 
-    @pytest.mark.parametrize("handle_sys_args", [["-inverted"]], indirect=True)
+    @pytest.mark.parametrize(
+        "handle_sys_args", [[TEST_IMAGE_PATH, "--inverted"]], indirect=True
+    )
     def test_options_inverted(self, handle_sys_args):
         options = Options()
 
         assert options.inverted_colors
 
     @pytest.mark.parametrize(
-        "handle_sys_args", [["--output_path=./test/123/"]], indirect=True
+        "handle_sys_args",
+        [[TEST_IMAGE_PATH, "--output_path", "./test/123/"]],
+        indirect=True,
     )
     def test_options_output_path(self, handle_sys_args):
         options = Options()
 
         assert options.output_path == "./test/123/"
 
-    @pytest.mark.parametrize("handle_sys_args", [["test123.mp4"]], indirect=True)
+    @pytest.mark.parametrize("handle_sys_args", [[TEST_VIDEO_PATH]], indirect=True)
     def test_options_input_video_file(self, handle_sys_args):
         options = Options()
 
         assert len(options.input) == 1
-        assert options.input[0].name == "test123"
+        assert options.input[0].name == "test"
         assert options.input[0].extension == "mp4"
-        assert options.input[0].get_name_with_extension() == "test123.mp4"
+        assert options.input[0].get_name_with_extension() == "test.mp4"
         assert options.input[0].type == FileType.Video
 
-    @pytest.mark.parametrize(
-        "handle_sys_args", [["./test/resource/img/input/test.jpg"]], indirect=True
-    )
+    @pytest.mark.parametrize("handle_sys_args", [[TEST_IMAGE_PATH]], indirect=True)
     def test_options_input_image_file(self, handle_sys_args):
         options = Options()
 
@@ -81,10 +83,3 @@ class TestOptions:
         assert options.input[0].extension == "jpg"
         assert options.input[0].get_name_with_extension() == "test.jpg"
         assert options.input[0].type == FileType.Image
-
-    @pytest.mark.parametrize("handle_sys_args", [["-help"]], indirect=True)
-    def test_options_input_video_file(self, handle_sys_args):
-        with pytest.raises(SystemExit) as sys_exit:
-            Options()
-
-        assert sys_exit.value.args[0] == HELP_MESSAGE
